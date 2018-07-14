aea7946f0876c026046d33d83c9c7f0fd271ee2b
Gustavo B. Knapp <gustavo.bauer.knapp@gmail.com>
2018-07-14 02:05:25 -0300

    Added structural files

--- music/apps.py
+++ music/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class TutoConfig(AppConfig):
+class MusicConfig(AppConfig):
     name = 'music'
--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('^$', views.index, name='index'),
+    # /music/
+    path('', views.index, name='index'),
+    # /music/123/
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
